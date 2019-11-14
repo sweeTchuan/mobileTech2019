2656830ae9898db4fd9d5935c89bad46b92db3cf
sweeTchuan <chuan321@gmail.com>
2019-11-14 22:26:04 +0800

    add files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,14 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'test-page', loadChildren: './test-page/test-page.module#TestPagePageModule' },
+  { path: 'tp2', loadChildren: './tp2/tp2.module#Tp2PageModule' },
+  { path: 'tp3', loadChildren: './tp3/tp3.module#Tp3PageModule' },
+  { path: 'tp-activity', loadChildren: './tp-activity/tp-activity.module#TpActivityPageModule' },
+  { path: 'tp-profile', loadChildren: './tp-profile/tp-profile.module#TpProfilePageModule' },
+  { path: 'tp-login', loadChildren: './tp-login/tp-login.module#TpLoginPageModule' },
+  { path: 'tp-register', loadChildren: './tp-register/tp-register.module#TpRegisterPageModule' },
+  { path: 'tp-add-post', loadChildren: './tp-add-post/tp-add-post.module#TpAddPostPageModule' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,21 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
+import { HttpClientModule } from '@angular/common/http';
+import { HTTP } from '@ionic-native/http/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
+    NativeGeocoder,
+    HTTP,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
